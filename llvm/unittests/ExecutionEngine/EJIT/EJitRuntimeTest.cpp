c37013ff64cc4cb4949355dc3272316b6cdb1e60
ChenRuan <145383140+ChenRuan@users.noreply.github.com>
2026-08-14 14:25:04 +0100

    [EJIT] Harden shared online PGO for SRE
    Rebased onto the current ejit_dev_spec4 baseline while preserving current diagnostics.

--- llvm/unittests/ExecutionEngine/EJIT/EJitRuntimeTest.cpp
+++ llvm/unittests/ExecutionEngine/EJIT/EJitRuntimeTest.cpp
@@ -2653,7 +2653,8 @@ TEST(EJitInterprocedural, ConstantsStopAtCallEdgeWithoutIPSCCP) {
   EJitOptimizerTestAccess opt(reg);
   opt.runInstCombine(*M);
   opt.runStructFieldPass(*M);
-  opt.runOptimizationPipeline(*M, llvm::ejit::OptimizationLevel::L2);
+  opt.runOptimizationPipeline(*M, llvm::ejit::OptimizationLevel::L2,
+                              CompileTier::Baseline);
 
   Function *Callee = M->getFunction("ip_callee");
   ASSERT_NE(Callee, nullptr);
@@ -2682,7 +2683,8 @@ TEST(EJitInterprocedural, IPSCCPPropagatesDimsIntoCallee) {
   opt.runInterproceduralPropagation(*M);
   opt.runInstCombine(*M);
   opt.runStructFieldPass(*M);
-  opt.runOptimizationPipeline(*M, llvm::ejit::OptimizationLevel::L2);
+  opt.runOptimizationPipeline(*M, llvm::ejit::OptimizationLevel::L2,
+                              CompileTier::Baseline);
 
   Function *Callee = M->getFunction("ip_callee");
   Function *Entry = M->getFunction("ip_entry");
